d407bcae954aa114a054c3990bfddd0e4d6864da
pianvarz <74831886+pianvarz@users.noreply.github.com>
2021-07-20 20:05:38 +0100

    Add files via upload

--- backend/app.js
+++ backend/app.js
@@ -42,3 +42,7 @@ app.use('/api/auth', userRoutes);
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
+
+//Created accounts - Username: Nando@mail.com; Password: Password12
+                            // Ferdinand@mail.com        Ferdinand247
+                            // olivares@mail.com         Olivares24
